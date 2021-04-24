5646f092dafcd9f3597091e84e06e1e60431daa8
Carlos <carlos.alfonso.montero@gmail.com>
2021-04-24 14:02:19 -0400

    Show data detail from data parent

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const BASE_URL = "http://lookup-service-prod.mlb.com/json/";
+/*this variable is to identify the info detail to show
+0 - default => Show roster
+1 - show player info
+*/
+let option = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
     showData();
-    
 })
 
 ///////////////////////FECTH ZONE////////////////////////////////////
@@ -42,20 +46,57 @@ const getRoster = (idTeam) => {
     })
 }
 
-function getDataPlayerByName(name) {
-    const url = `${BASE_URL}named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='cabrera%25'`;
+function getPlayerById(idPlayer) {
+    const url = `${BASE_URL}named.player_info.bam?sport_code='mlb'&player_id=${idPlayer}`;
     return fetch(url , {
         method: "GET",
         headers: {
             "content-type": "application/json"
         }
-    }) 
+    })
     .then(res => res.json())
+    .then(res => {
+        const player_info = res.roster_40;
+        const queryResults = player_info.queryResults;
+        const row = queryResults.row;
+        const teamsArray = [...row];
+        showPlayerInfo(teamsArray);
+    })
 }
+
 /////////////////////////////////////////////////////////////////////
 
 function showData() {
     getDataTeam();
+}
+
+function createTableInfo() {
+
+        const table = document.createElement('table');
+        table.setAttribute("id", "table-body2");
+        const tr = document.createElement('tr');
+
+        const row = table.insertRow(-1);
+        const headerCell0 = document.createElement("TH");
+        const headerCell1 = document.createElement("TH");
+        headerCell0.innerHTML = "Name";
+        headerCell1.innerHTML = "Position";
+        row.appendChild(headerCell0);
+        row.appendChild(headerCell1);
+
+        //Add data from Array
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+
+        table.appendChild(tr);
+        document.body.appendChild(table);  
+}
+
+
+function showPlayerInfo(objPlayer) {
+    
 }
 
 function showDataTeam(objTeam) {
@@ -67,6 +108,7 @@ function showDataTeam(objTeam) {
 }
 
 function showTeamRoster(objTeam) {
+    createTableInfo();
     const rosterContainer = document.querySelector("#table-body2");
     objTeam.forEach(obj => {
         const rosterTable = createTableRoster(obj);
@@ -78,11 +120,19 @@ function createTableRoster(obj) {
     const tr = document.createElement("tr");
     const tdName = document.createElement("td");
     const tdPosition = document.createElement("td");
+        //Create Button to select player
+        const selectBttn =  document.createElement("td"),
+        playerBttn = document.createElement("button");
+        playerBttn.innerText = 'Select';
+        playerBttn.dataset.id = obj.player_id;
+        playerBttn.addEventListener('click', handleClick);
+        playerBttn.myParam = 1;
+        selectBttn.append(playerBttn);
 
     tdName.textContent = obj.name_full;
     tdPosition.textContent= obj.position_txt;
 
-    tr.append(tdName,tdPosition);
+    tr.append(tdName,tdPosition,selectBttn);
     return tr;
 }
 
@@ -97,6 +147,7 @@ function createTableTeam(obj) {
     teamBttn.innerText = 'Select';
     teamBttn.dataset.id = obj.team_id;
     teamBttn.addEventListener('click', handleClick);
+    teamBttn.myParam = 0;
     selectBttn.append(teamBttn);
 
     tdName.textContent = obj.name_display_full;
@@ -113,9 +164,30 @@ function createTableTeam(obj) {
 }
 
 function handleClick(e) {
+    //clearContainer();
+    let option = e.target.myParam
     let id = e.target.dataset.id;
-    getRoster(id);
+
+    if (option === 0) {    
+        getRoster(id);
+    } else {
+        console.log(id);
+    }
 }
+
+function clearContainer() {
+    var elementExists = document.getElementById("table-body2");
+    if (elementExists != null) {
+        const listEmpty = elementExists.innerHTML.trim();
+        if (listEmpty != "") {
+            elementExists.innerHTML = ""; 
+        }
+    }
+}
+    
+
+
+
 /*
 const teamContainer = document.querySelector("#table-body");
 teamContainer.addEventListener("click", (e) {
